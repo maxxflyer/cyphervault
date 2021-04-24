a74e5977dd6cfc22174b0273abce92ff3650ae2a
maxxflyer <maxxflyer@gmail.com>
2021-04-24 13:06:24 +0200

    Add files via upload

---
+++ precache-manifest.88119ceff554e848efbbe7181127d0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de16c14221a9e739aab6817d3ba26478",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dec3315a9055f6e7599d",
+    "url": "./static/css/main.2853ad0d.chunk.css"
+  },
+  {
+    "revision": "7e35a6d21f08c57eef53",
+    "url": "./static/js/2.963e7d6b.chunk.js"
+  },
+  {
+    "revision": "3784163517bed8ce1941c739bda8c3bb",
+    "url": "./static/js/2.963e7d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec3315a9055f6e7599d",
+    "url": "./static/js/main.6d59d00d.chunk.js"
+  },
+  {
+    "revision": "9da974e1e1b30758e469",
+    "url": "./static/js/runtime-main.af73ffaf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d8ee534cf90d31fb96c22de1a6abc1f0.js"
+  "./precache-manifest.88119ceff554e848efbbe7181127d0a5.js"
 );
 
 self.addEventListener('message', (event) => {
